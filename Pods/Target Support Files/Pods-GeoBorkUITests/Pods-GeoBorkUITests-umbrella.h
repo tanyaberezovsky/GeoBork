e10e3c7c09b7c5d6b19dc8f69f261531f9084bf1
Tania Berezovski <cocakolata@gmail.com>
2017-02-10 14:52:03 +0200

    deleting pods dir

--- Pods/Target Support Files/Pods-GeoBorkUITests/Pods-GeoBorkUITests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_GeoBorkUITestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_GeoBorkUITestsVersionString[];
-
